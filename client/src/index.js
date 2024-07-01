f68e854752dd0960f8225473951b6ff61e396cc5
rachaen <rachaenlee@gmail.com>
2024-07-01 16:54:06 +0900

    feat: 페이지 라우팅 적용

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Join from './pages/Join';
+import ChatRoom from './pages/ChatRoom';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Join />,
+  },
+  {
+    path: '/chat',
+    element: <ChatRoom />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
