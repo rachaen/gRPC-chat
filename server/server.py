087951ae93903dc74a17a8f000ac1f79cef3f9db
jyjang <jyjang@suresofttech.com>
2024-07-05 13:57:30 +0900

    feat : import 경로 수정, server 구동 구현

--- server/server.py
+++ server/server.py
@@ -1,26 +1,32 @@
-import sys, os
+import sys, os, time
 import logging
 from concurrent import futures
-
 import grpc
+
 sys.path.append(os.path.join(os.path.dirname(__file__), 'proto'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'services'))
 
-from proto import server_pb2, server_pb2_grpc
-
-
-class ServerServicer(server_pb2_grpc.ServerServicer):
-    def GetServerResponse(self, request, context):
-        response_message = f"Received your message: {request.message}"
-        return server_pb2.MessageResponse(message=response_message, received=True)
+import server_pb2
+import server_pb2_grpc
+from MessageService import MessageService
+from UserService import UserService
 
 def serve():
     logging.info('Starting server...')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    server_pb2_grpc.add_ServerServicer_to_server(ServerServicer(), server)
+    server_pb2_grpc.add_UserServiceServicer_to_server(UserService(), server)
+    server_pb2_grpc.add_UserServiceServicer_to_server(MessageService(), server)
+
     server.add_insecure_port('[::]:50051')
     server.start()
     logging.info('Server started on port 50051')
-    server.wait_for_termination()
+
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        server.stop(0)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
--- server/services/UserService.py
+++ server/services/UserService.py
@@ -4,8 +4,10 @@ import grpc
 from concurrent import futures
 import time
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'proto'))
-from proto import server_pb2, server_pb2_grpc
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'proto'))
+
+import server_pb2
+import server_pb2_grpc
 
 
 class UserService(server_pb2_grpc.UserServiceServicer):
